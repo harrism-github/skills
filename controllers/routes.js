6f71e15ff92676eb77e98f1ff683c50914bae4dc
MIchael Harris <harrism@ldschurch.org>
2013-08-13 10:33:53 -0600

    Initial checkin.

--- controllers/routes.js
+++ controllers/routes.js
@@ -1,14 +1,18 @@
 module.exports = function(app) {
   
-  /* Optionally include this to use this alternative template when wanting to create your own header and footer
+  //Optionally include this to use this alternative template when wanting to create your own header and footer
   app.set("view options", {
     layout: "layout/layout-skills"
   });
-  */
-  
+
+  app.get('/todo', function(req, res){
+     res.render("todo", {});
+  });
+
   app.get('/:page', function(req, res){
     res.render(req.params.page, {});
   });
+
   app.get('/', function(req, res){
     res.render("index", {});
   });
